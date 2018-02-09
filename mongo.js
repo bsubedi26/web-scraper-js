a49579ef120f738d5420104a05b2f07094903de0
Bibek Subedi <bsubedi26@gmail.com>
2018-02-08 22:38:23 -0500

    mongodb initialize

---
+++ mongo.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require('mongodb');
+///////////////////////////////////////////////////////
+///////////////////// MONGO DB ////////////////////////
+///////////////////////////////////////////////////////
+async function initDatabase() {
+
+  const url = 'mongodb://localhost:27017';
+  const dbName = 'scraper';
+
+  const client = await MongoClient.connect(url)
+  console.log("MongoDB connected successfully!");
+  const db = client.db(dbName);
+  db.client = client;
+  return db;
+}
+
+
+module.exports = initDatabase;
